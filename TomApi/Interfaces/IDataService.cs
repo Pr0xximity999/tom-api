88e2f684705e5902492cda41405b2f47c235b9b3
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-02-24 11:08:21 +0100

    moved docstrings

--- TomApi/Interfaces/IDataService.cs
+++ TomApi/Interfaces/IDataService.cs
@@ -2,7 +2,30 @@ namespace TomApi.Interfaces;
 
 public interface IDataService
 {
+    /// <summary>
+    /// Executes the given query on an azure database and returns the result in a list.
+    /// Use QueryFirstSql or .FirstOrDefault() in case  you expect only a single object
+    /// </summary>
+    /// <param name="query">The ran query</param>
+    /// <param name="parameters">The added query params</param>
+    /// <typeparam name="T">The type inside the collection the response will be casted to</typeparam>
+    /// <returns></returns>
     public IEnumerable<T> QuerySql<T>(string query, object? parameters = null);
+    
+    /// <summary>
+    /// Executes the given query on an azure database and returns the first result in a list.
+    /// </summary>
+    /// <param name="query">The ran query</param>
+    /// <param name="parameters">The added query params</param>
+    /// <typeparam name="T">The type inside the collection the response will be casted to</typeparam>
+    /// <returns></returns>
     public T QueryFirstSql<T>(string query, object? parameters = null);
+    
+    /// <summary>
+    /// Executes the given query on an azure database and returns wether one or more row changed
+    /// </summary>
+    /// <param name="query">The ran query</param>
+    /// <param name="parameters">The added query params</param>
+    /// <returns></returns>
     public bool ExecuteSql(string query, object? parameters = null);
 }
--- TomApi/Services/MySqlDataService.cs
+++ TomApi/Services/MySqlDataService.cs
@@ -3,7 +3,12 @@ using MySql.Data.MySqlClient;
 using TomApi.Interfaces;
 
 namespace TomApi.Services;
-
+    /// <summary>
+    /// Executes the given query and returns wether one or more row changed
+    /// </summary>
+    /// <param name="query">The ran query</param>
+    /// <param name="parameters">The added query params</param>
+    /// <returns></returns>
 public class MySqlDataService : IDataService
 {
     private IConfiguration _config;
@@ -12,15 +17,7 @@ public class MySqlDataService : IDataService
     {
         _config = config;
     }
-    
-    /// <summary>
-    /// Executes the given query and returns the result in a list.
-    /// Use .FirstOrDefault() in case  you expect only a single object
-    /// </summary>
-    /// <param name="query">The ran query</param>
-    /// <param name="parameters">The added query params</param>
-    /// <typeparam name="T">The type inside the collection the response will be casted to</typeparam>
-    /// <returns></returns>
+
     public IEnumerable<T> QuerySql<T>(string query, object? parameters=null)
     {
         using var connection = new MySqlConnection(_config.GetConnectionString(CONN) ?? "");
@@ -28,22 +25,11 @@ public class MySqlDataService : IDataService
 
         return result;
     }
-
-    /// <summary>
-    /// Executes the given query on an azure database and returns the first result in a list.
-    /// </summary>
-    /// <param name="query">The ran query</param>
-    /// <param name="parameters">The added query params</param>
-    /// <typeparam name="T">The type inside the collection the response will be casted to</typeparam>
-    /// <returns></returns>
+    
+    
     public T QueryFirstSql<T>(string query, object? parameters = null) => QuerySql<T>(query, parameters).First();
     
-    /// <summary>
-    /// Executes the given query and returns wether one or more row changed
-    /// </summary>
-    /// <param name="query">The ran query</param>
-    /// <param name="parameters">The added query params</param>
-    /// <returns></returns>
+    
     public bool ExecuteSql(string query, object? parameters=null)
     {
         using var connection = new MySqlConnection(_config.GetConnectionString(CONN) ?? "");
